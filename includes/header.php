97a5d703c0dfee6b9e1ab4f3dabd3e30f7d65476
Arnaud LECOMTE <AL@MacBook-Pro-de-Arnaud.local>
2017-04-30 02:37:54 +0200

    j'en peux deja plus ...

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<div class="container">
+		<div><a href="index.php" id="title-text">Camagru</a></div>
+		<div class="button"><button id="log_in-button">Log in</button></div>
+		<div class="button"><button id="log_in-button">Sign up</button></div>
+	</div>
+</body>
+</html>
