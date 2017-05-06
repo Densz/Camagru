52c4a536c6b6bac6ce10921077ecd01596ea6535
Denis ZHENG <dzheng@e2r11p3.42.fr>
2017-05-06 13:27:09 +0200

    ok

--- public/index.php
+++ public/index.php
@@ -1,5 +1,27 @@
 <?php
+    define('ROOT', dirname(__DIR__));
+    include(ROOT . '/app/Views/templates/default.php');
 
-echo phpversion();
+    if (isset($_GET['p'])){
+        $page = $_GET['p'];
+    } else if ($_SESSION['auth'] !== null) {
+        $page = 'index';
+    } else {
+        $page = 'login';
+    }
 
+    // if (isset($_GET['p']))
+    // {
+    //     $page = $_GET['p'];
+    // }
+    // else if ($_SESSION('auth') !== null)
+    // {
+    //     $page = 'index';
+    // } 
+    // else
+    // {
+    //     $page = 'login';
+    // }
+
+    if ($page == '')
 ?>
