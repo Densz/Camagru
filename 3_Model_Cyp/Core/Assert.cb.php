2669d1d28c1aab42e6489093f8b55fadb04125af
God <zheng.denis@gmail.com>
2017-05-09 23:20:52 +0200

    Redirection

--- 3_Model_Cyp/Core/Assert.cb.php
+++ 3_Model_Cyp/Core/Assert.cb.php
@@ -2,12 +2,12 @@
 
 class CB
 {
-    public static function my_assert($str)
-    {
-        if (isset($str) && !empty($str))
-            return (1);
-        return (0);
-    }
+	public static function my_assert($str)
+	{
+		if (isset($str) && !empty($str))
+			return (1);
+		return (0);
+	}
 }
 
 ?>
--- 3_Model_Cyp/Core/Routeur.class.php
+++ 3_Model_Cyp/Core/Routeur.class.php
@@ -2,17 +2,17 @@
 
 class Routeur
 {
-    public static $url = null;
+	public static $url = null;
 
-    public function __construct()
-    {
-        $uri = $_SERVER['REQUEST_URI'];
-        $url = explode('/', $uri);
-        (CB::my_assert($url[3])) ? Routeur::$url['controller'] = $url[3] : print_r("redirection 404");
-        (CB::my_assert($url[4])) ? Routeur::$url['function'] = $url[4] : print_r("redirection 404");
-        for ($i = 5; $i <= count($url) - 1; $i++)
-            Routeur::$url['params'][] = $url[$i];
-    }
+	public function __construct()
+	{
+		$uri = $_SERVER['REQUEST_URI'];
+		$url = explode('/', $uri);
+		(CB::my_assert($url[3])) ? Routeur::$url['controller'] = $url[3] : header('Location: http://localhost:8888/camagru/3_Model_Cyp/visitorsignup/view/');
+		(CB::my_assert($url[4])) ? Routeur::$url['function'] = $url[4] : print_r("redirection 404");
+		for ($i = 5; $i <= count($url) - 1; $i++)
+			Routeur::$url['params'][] = $url[$i];
+	}
 }
 
 ?>
