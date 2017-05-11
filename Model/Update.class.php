e23c80d2daf58c4487f1008fdfb974b7b7b94f0d
Arnaud LECOMTE <arlecomt@e2r7p2.42.fr>
2017-05-11 13:16:42 +0200

    ciao test

--- Model/Update.class.php
+++ Model/Update.class.php
@@ -2,11 +2,6 @@
 class Update
 {
 
-	public function __cosntruct()
-	{
-		echo test;
-	}
-
     static function accountConfirmed($login){
 		$database = new Core\MysqlDb("mysql:dbname=camagru;host=localhost", $DB_USER, $DB_PASSWORD);
         $database->query("UPDATE users SET email_confirmed = 'yes' WHERE login = '$login'");
