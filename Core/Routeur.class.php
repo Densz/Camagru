d954555a78f212b9623f8acebf9157c6ee1d95d3
Arnaud LECOMTE <arlecomt@e2r4p2.42.fr>
2017-05-13 14:28:05 +0200

    fixe admin account and redirect home

--- Core/Routeur.class.php
+++ Core/Routeur.class.php
@@ -9,7 +9,7 @@ class Routeur
 		$uri = $_SERVER['REQUEST_URI'];
 		$url = explode('/', $uri);
 		Routeur::$url['dir'] = $url[1];
-		(CB::my_assert($url[2])) ? Routeur::$url['controller'] = $url[2] : print_r("redirection 404");
+		(CB::my_assert($url[2])) ? Routeur::$url['controller'] = $url[2] : (CB::my_assert($_SESSION['auth']) ? $this->redirect("Userindex/view") : $this->redirect("Authsignin/signIn"));
 		(CB::my_assert($url[3])) ? Routeur::$url['method'] = $url[3] : print_r("redirection 404");
 		for ($i = 4; $i <= count($url) - 1; $i++)
 			Routeur::$url['params'][] = $url[$i];
